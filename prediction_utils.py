5fc5879f555c5311720a867d297d498a438120a2
Lee Hyunju <guswnee00@gmail.com>
2023-08-30 10:28:07 +0900

    ver.1

--- prediction_utils.py
+++ prediction_utils.py
@@ -12,7 +12,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # 모델을 불러오는 함수
-def load_yolo_model(model_path = '/Users/hyunjulee/tp2/TrafficDetection/model/best_1.pt'):
+def load_yolo_model(model_path = '/Users/hyunjulee/tp2/tp2_flask_2/model/best_1.pt'):
     
     model = YOLO(model_path)
  
@@ -20,7 +20,7 @@ def load_yolo_model(model_path = '/Users/hyunjulee/tp2/TrafficDetection/model/be
 
 # 모델을 사용해서 예측하는 함수
 def predict_image(model, image_path):
-
+    
     # 이미지 읽어오기
     image = cv2.imread(image_path)
 
@@ -28,29 +28,29 @@ def predict_image(model, image_path):
     model.predict(image, imgsz=640, save = True)
 
     # predictions 이미지 경로
-    source_image_path = "/Users/hyunjulee/tp2/TrafficDetection/runs/detect/predict/image0.jpg"
-
+    source_image_path = "/Users/hyunjulee/tp2/tp2_flask_2/runs/detect/predict/image0.jpg"
+    
     # 원본 이미지의 파일 이름 추출
     original_image_name, _ = os.path.splitext(os.path.basename(image_path))
-
+    
     # 새로운 이름 생성 (원본 이미지 이름 앞에 'pred_'를 붙임)
     new_image_name = f'pred_{original_image_name}.jpg'
-
+    
     # 새로운 이미지 경로
     predictions = os.path.join("static", "predictions", new_image_name)
-
+    
     # 이미지 이름 변경 및 이동
     shutil.move(source_image_path, predictions)
 
     # 이동 후에는 디렉토리 삭제
-    directory_to_delete = "/Users/hyunjulee/tp2/TrafficDetection/runs"
+    directory_to_delete = "/Users/hyunjulee/tp2/tp2_flask_2/runs"
     try:
         shutil.rmtree(directory_to_delete)
         print(f"디렉토리 삭제 완료: {directory_to_delete}")
     except Exception as e:
         print(f"디렉토리 삭제 실패: {e}")
 
-    print(f'destination_image_path : {predictions}')
+    # 확인
+    #print(f'destination_image_path : {predictions}')
 
     return predictions
-    
